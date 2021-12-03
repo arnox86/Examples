cc6e09719666f9d6ffa72aa4da62f82fc2f3ef6f
Bene <63281428+arnox86@users.noreply.github.com>
2021-12-03 21:13:27 +0100

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+#define STRING_SIZE 100     // Maximum size of string
+
+
+int main () {
+
+    char input [STRING_SIZE];
+
+    cout << "Enter your calculation: ";
+    cin >> input;
+
+}
